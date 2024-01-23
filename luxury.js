2eef7fb0a626732f8efc4d9b185638b8531a61c4
Hamza-Foughali <hamzafougahli@gmail.com>
2024-01-23 10:33:48 +0100

    done

---
+++ luxury.js
@@ -0,0 +1,12 @@
+
+// $(document).ready(function () {
+//   var buttons = document.getElementsByClassName('buy-button');
+  
+//   $('#buy-button').click(function () {
+//     alert('You bought my watch');
+//   });
+//   console.log(buttons);
+// });
+ function add(id){
+  alert('You ' + id + "added to cart")
+ }
